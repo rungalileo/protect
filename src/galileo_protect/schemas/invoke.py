064fbb1302f68adbbf35de481cf5f36e21e9e69f
Setu Shah <setu@rungalileo.io>
2024-05-13 17:12:39 -0700

    fix: Make status optional

--- src/galileo_protect/schemas/invoke.py
+++ src/galileo_protect/schemas/invoke.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict, Field
 
 
 class Response(BaseModel):
     text: str = Field(description="Text from the request after processing the rules.")
-    status: str = Field(description="Status of the request after processing the rules.")
+    # Status is optional because it is just being added to the response. To maintain
+    # backwards compatibility, it is optional. We'll make it required in v0.9.0.
+    status: Optional[str] = Field(default=None, description="Status of the request after processing the rules.")
 
     model_config = ConfigDict(extra="allow")
