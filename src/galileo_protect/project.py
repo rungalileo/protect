a3d0328779079aa6eec56aa77b2cd6abe497cf3b
Setu Shah <setu@rungalileo.io>
2024-06-11 12:49:08 -0700

    feat(core): Wrap `get_project` from `galileo-core`

--- src/galileo_protect/project.py
+++ src/galileo_protect/project.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 from galileo_core.helpers.project import create_project as core_create_project
+from galileo_core.helpers.project import get_project as core_get_project
 from galileo_core.helpers.project import get_projects as core_get_projects
 from galileo_core.schemas.core.project import (
     DEFAULT_PROJECT_NAME,
@@ -8,6 +9,7 @@ from galileo_core.schemas.core.project import (
     ProjectResponse,
     ProjectType,
 )
+from pydantic import UUID4
 
 from galileo_protect.helpers.config import ProtectConfig
 
@@ -53,3 +55,45 @@ def get_projects(config: Optional[ProtectConfig] = None) -> List[ProjectResponse
     """
     config = config or ProtectConfig.get()
     return core_get_projects(config=config, project_type=ProjectType.protect)
+
+
+def get_project(
+    project_id: Optional[UUID4] = None,
+    project_name: Optional[str] = None,
+    config: Optional[ProtectConfig] = None,
+    raise_if_missing: bool = True,
+) -> Optional[ProjectResponse]:
+    """
+    Get a Protect project by either ID or name.
+
+    If both project_id and project_name are provided, project_id will take precedence.
+
+    Parameters
+    ----------
+    config : GalileoConfig
+        Configuration object for the Galileo client.
+    project_id : Optional[UUID4], optional
+        Project ID, by default None.
+    project_name : Optional[str], optional
+        Project name, by default None.
+    raise_if_missing : bool, optional
+        Raise an error if the project is not found, by default True.
+
+    Returns
+    -------
+    Optional[ProjectResponse]
+        Project response object if the project is found, None otherwise.
+
+    Raises
+    ------
+    ValueError
+        If neither project_id nor project_name is provided.
+    """
+    config = config or ProtectConfig.get()
+    return core_get_project(
+        config=config,
+        project_id=project_id,
+        project_name=project_name,
+        project_type=ProjectType.protect,
+        raise_if_missing=raise_if_missing,
+    )
