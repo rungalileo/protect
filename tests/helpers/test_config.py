838a7fd789bb0a086bba3d18c49f23a0ceb78848
Setu Shah <setu@rungalileo.io>
2024-06-19 01:08:30 -0700

    test: Refactor / update tests for new config

--- tests/helpers/test_config.py
+++ tests/helpers/test_config.py
@@ -1,12 +1,18 @@
+from typing import Callable
 from unittest.mock import Mock
 from uuid import uuid4
 
-from galileo_protect.helpers.config import ProtectConfig
-from tests.data import A_CONSOLE_URL, A_STAGE_NAME
+from pydantic import SecretStr
+
+from galileo_protect.schemas.config import ProtectConfig
+from tests.data import A_CONSOLE_URL, A_JWT_TOKEN, A_STAGE_NAME
 
 
-def test_reset(mock_healthcheck: Mock) -> None:
-    config = ProtectConfig(console_url=A_CONSOLE_URL, project_id=uuid4(), stage_name=A_STAGE_NAME, stage_id=uuid4())
+def test_reset(mock_healthcheck: Mock, mock_get_current_user: Mock) -> None:
+    ProtectConfig.set(
+        console_url=A_CONSOLE_URL, jwt_token=A_JWT_TOKEN, project_id=uuid4(), stage_name=A_STAGE_NAME, stage_id=uuid4()
+    )
+    config = ProtectConfig.get()
     assert config.console_url.unicode_string() == A_CONSOLE_URL
     assert config.project_id is not None
     assert config.stage_name == A_STAGE_NAME
@@ -16,3 +22,13 @@ def test_reset(mock_healthcheck: Mock) -> None:
     assert config.project_id is None
     assert config.stage_name is None
     assert config.stage_id is None
+
+
+def test_global_config(mock_healthcheck: Mock, mock_get_current_user: Mock, set_validated_config: Callable) -> None:
+    set_validated_config()
+    config = ProtectConfig.get()
+    assert config.console_url.unicode_string() == A_CONSOLE_URL
+    assert config.jwt_token == SecretStr(A_JWT_TOKEN)
+    assert config.project_id is None
+    assert config.stage_id is None
+    assert config.stage_name is None
--- tests/test_project.py
+++ tests/test_project.py
@@ -6,11 +6,12 @@ from galileo_core.constants.routes import Routes as CoreRoutes
 from galileo_core.schemas.core.project import ProjectType
 
 from galileo_protect.project import create_project, get_projects
+from galileo_protect.schemas.config import ProtectConfig
 from tests.data import A_PROJECT_NAME
 
 
 def test_create_project(set_validated_config: Callable, mock_request: Callable) -> None:
-    config = set_validated_config()
+    set_validated_config()
     project_id = uuid4()
     matcher_get = mock_request(
         RequestMethod.GET, CoreRoutes.projects, params=dict(project_name=A_PROJECT_NAME), json=[]
@@ -20,16 +21,17 @@ def test_create_project(set_validated_config: Callable, mock_request: Callable) 
         CoreRoutes.projects,
         json={"id": str(project_id), "type": ProjectType.protect, "name": A_PROJECT_NAME},
     )
-    create_project(name=A_PROJECT_NAME, config=config)
+    create_project(name=A_PROJECT_NAME)
     assert matcher_get.called
     assert matcher_post.called
     # Verify that the project ID was set in the config.
+    config = ProtectConfig.get()
     assert config.project_id is not None
     assert config.project_id == project_id
 
 
 def test_get_projects(set_validated_config: Callable, mock_request: Callable) -> None:
-    config = set_validated_config()
+    set_validated_config()
     project_id = uuid4()
     matcher_get = mock_request(
         RequestMethod.GET,
@@ -37,7 +39,7 @@ def test_get_projects(set_validated_config: Callable, mock_request: Callable) ->
         json=[{"id": str(project_id), "type": ProjectType.protect, "name": A_PROJECT_NAME}],
     )
 
-    projects = get_projects(config=config)
+    projects = get_projects()
     assert matcher_get.called
     assert len(projects) == 1
     assert projects[0].id == project_id
