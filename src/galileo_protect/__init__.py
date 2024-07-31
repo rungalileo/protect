05c4790fdbd3ef22594d0b0af2c8b440b79f78ff
Setu Shah <setu@rungalileo.io>
2024-07-31 14:46:26 -0700

    chore(schemas): Export `SubscriptionConfig` from `galleo_protect` (#89)

--- src/galileo_protect/__init__.py
+++ src/galileo_protect/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa: F401
 # ruff: noqa: F401
 from galileo_core.helpers.dependencies import is_dependency_available
+from galileo_core.schemas.protect.subscription_config import SubscriptionConfig
 
 from galileo_protect.invocation import ainvoke, invoke
 from galileo_protect.project import create_project, get_project, get_projects
