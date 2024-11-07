95082f9dc2e9b51b862285bba19c0f331ddeb36b
Setu Shah <setu@rungalileo.io>
2024-11-07 10:18:32 -0800

    feat: Pass in project name to invocation requests (#140)

--- src/galileo_protect/schemas/config.py
+++ src/galileo_protect/schemas/config.py
@@ -11,8 +11,9 @@ class Config(BaseConfig):
 
     # Protect specific configuration.
     project_id: Optional[UUID4] = None
-    stage_name: Optional[str] = None
+    project_name: Optional[str] = None
     stage_id: Optional[UUID4] = None
+    stage_name: Optional[str] = None
     stage_version: Optional[int] = None
 
     def reset(self) -> None:
